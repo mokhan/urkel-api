9fcc169a52dc85da1697275ee9904b0aabd87b39
mo khan <mo@mokhan.ca>
2014-04-08 22:00:08 -0600

    add specs for authenticate

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,8 +3,20 @@ require "spec_helper"
 describe User do
   context "#save" do
     it "can be saved" do
-      User.create!(email: 'mo@mokhan.ca')
+      User.create!(email: 'mo@mokhan.ca', password: 'password', password_confirmation: 'password')
       User.count.should == 1
+    end
+  end
+
+  context "#authenticate" do
+    let(:user) { User.create!(email: 'blah@example.com', password: 'password', password_confirmation: 'password') }
+
+    it "returns true when the password is correct" do
+      user.authenticate('password').should be_true
+    end
+
+    it "returns false when the password is incorrect" do
+      user.authenticate('wrong').should be_false
     end
   end
 end
