d1aa4c6c0f292d5961d2a13cb20b63151c28be0d
mo khan <mo@mokhan.ca>
2014-11-15 08:40:36 -0700

    let the container build the resource.

--- config/initializers/container_configuration.rb
+++ config/initializers/container_configuration.rb
@@ -1,5 +1,4 @@
 container = Spank::Container.new
-container.register(:login_command) do |builder|
-  LoginCommand.new(User)
-end
+container.register(:login_command) { |x| x.build(LoginCommand) }
+container.register(:users) { |x| User }.as_singleton
 Spank::IOC.bind_to(container)
