916fdef394efdb546c18b61f031dec6f620863d3
mo khan <mo@mokhan.ca>
2014-11-15 17:16:04 -0700

    add spec for when the email is not known.

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -3,14 +3,16 @@ require 'rails_helper'
 describe "the signin process", type: :feature do
   let!(:user) { create(:user, password: 'password') }
 
-  it 'signs the user in' do
-    visit root_path
-    within ".form-signin" do
-      fill_in 'email', with: user.email
-      fill_in 'password', with: 'password'
+  context "when the credentials are correct" do
+    it 'signs the user in' do
+      visit root_path
+      within ".form-signin" do
+        fill_in 'email', with: user.email
+        fill_in 'password', with: 'password'
+      end
+      click_button "Sign in"
+      expect(page).to have_content("Dashboard")
     end
-    click_button "Sign in"
-    expect(page).to have_content("Dashboard")
   end
 
   context "when the password is incorrect" do
@@ -19,6 +21,18 @@ describe "the signin process", type: :feature do
       within ".form-signin" do
         fill_in 'email', with: user.email
         fill_in 'password', with: 'wrong'
+      end
+      click_button "Sign in"
+      expect(page).to have_content(I18n.translate(:invalid_credentials))
+    end
+  end
+
+  context "when the email is unknown" do
+    it 'displays an error' do
+      visit root_path
+      within ".form-signin" do
+        fill_in 'email', with: "test@example.com"
+        fill_in 'password', with: 'password'
       end
       click_button "Sign in"
       expect(page).to have_content(I18n.translate(:invalid_credentials))
